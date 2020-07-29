9a7f947946573ef46299b8069529091c436d69ba
iceskatercodes <aravindisiceskater@gmail.com>
2020-07-29 13:18:30 +0530

    minor fixes

---
+++ Week2/26-07/openapi/open.js
@@ -0,0 +1,9 @@
+
+var request = new XMLHttpRequest();
+var url = 'https://dog.ceo/api/breeds/list/all'
+request.open('GET', url, true);
+request.send();
+request.onload = function(){
+   var data = JSON.parse(this.response);
+console.log("data",data);
+}
--- Week2/26-07/openapi/openAPI.js
+++
@@ -1,7 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://dog.ceo/api/breeds/list/all', true);
-request.send();
-request.onload = function(){
-var data = JSON.parse(this.response);
-console.log(data);
-}
